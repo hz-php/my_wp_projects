9c79168f317beb881cc910ab140474837d8a5be7
sia041081 <sia041081@gmail.com>
2023-12-08 21:05:49 +0300

    first commit

---
+++ wp-content/themes/aquila/footer.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Footer file.
+ *
+ * @package Aquila
+ */
+?>
+<footer>footer</footer>
+</div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
+
---
+++ wp-content/themes/aquila/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Main template file.
+ *
+ * @pockage Aquila
+ */
+
+get_header()
+?>
+
+<div class="content">Content</div>
+
+<?php
+
+get_footer()
+?>
